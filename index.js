afc414300e508142efb51a461954f9470bb40c4e
Geeta rawat <rwtgeet6@gmail.com>
2025-05-28 10:10:11 +0530

    fetch user

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const express=require("express")
-const app=express()
-const PORT=8000
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
-app.listen(PORT,()=>{
+const members = [
+  {
+    id: 1,
+    name: "Deepak Singh Rawat",
+    email: "dsrwt107@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Deepak Singh ",
+    email: "dsingh@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Deepak ",
+    email: "dpk@gmail.com",
+    status: "active",
+  },
+];
+
+app.get("/showAllUser", (req, res) => {
+  res.status(200).json(members);
+});
+
+app.get("/showUser/:userId", (req, res) => {
+  // console.log(typeof req.params.userId);
+
+  const id = parseInt(req.params.userId);
+  let user = members.filter((member) => member.id === id);
+  user.length !== 0
+    ? res.status(200).json(user)
+    : res.status(404).json({ message: `User  not found with id ${id}` });
+
+  // if (user.length !== 0) res.status(200).json(user);
+  // else res.status(404).json({ message: `User  not found with id ${id}` });
+});
+
+app.listen(PORT, () => {
   console.log(`server in running on port http://localhost:${PORT}`);
-  
-})
+});
